95501834cdacc3dd863325dce45ac79dfcad051f
zgustn97 <zgustn0@gmail.com>
2021-12-30 20:38:11 +0900

    feat: 회원가입 파이어베이스 연동
    회원가입시 파이어베이스 Auth에 전화번호로 사용자가 추가됨

--- app/src/main/java/com/example/makerenew/Signup.kt
+++ app/src/main/java/com/example/makerenew/Signup.kt
@@ -36,14 +36,25 @@ class Signup : AppCompatActivity() {
         var storedVerificationId = ""
         var resendToken : PhoneAuthProvider.ForceResendingToken
 
+        fun signInWithPhoneAuthCredential(credential: PhoneAuthCredential) {
+            auth.signInWithCredential(credential)
+                .addOnCompleteListener(this@Signup) { task ->
+                    if (task.isSuccessful) {
+                        // Sign in success, update UI with the signed-in user's information
+                        Log.d(TAG, "signInWithCredential:success")
 
-
-        btn_smsSent.setOnClickListener(){
-            val options = PhoneAuthOptions.newBuilder(auth)
-                .setPhoneNumber("+82"+et_phone.text.toString()) //국가 번호 +82 를 해줬음
-                .setTimeout(60L, TimeUnit.SECONDS)
-                .setActivity(this)
-                .setCallbacks(object : PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+                        val user = task.result?.user
+                    } else {
+                        // Sign in failed, display a message and update the UI
+                        Log.w(TAG, "signInWithCredential:failure", task.exception)
+                        if (task.exception is FirebaseAuthInvalidCredentialsException) {
+                            // The verification code entered was invalid
+                        }
+                        // Update UI
+                    }
+                }
+        }
+        var callbacks = object : PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
 
 
 
@@ -73,55 +84,55 @@ class Signup : AppCompatActivity() {
             }
 
 
-                    override fun onCodeSent(
-                        verificationId: String,
-                        token: PhoneAuthProvider.ForceResendingToken
-                    ) {
-                        // The SMS verification code has been sent to the provided phone number, we
-                        // now need to ask the user to enter the code and then construct a credential
-                        // by combining the code with a verification ID.
-                        Log.d(TAG, "onCodeSent:$verificationId")
+            override fun onCodeSent(
+                verificationId: String,
+                token: PhoneAuthProvider.ForceResendingToken
+            ) {
+                // The SMS verification code has been sent to the provided phone number, we
+                // now need to ask the user to enter the code and then construct a credential
+                // by combining the code with a verification ID.
+                Log.d(TAG, "onCodeSent:$verificationId")
 
-                        // Save verification ID and resending token so we can use them later
-                        storedVerificationId = verificationId
-                        resendToken = token
-
-
+                // Save verification ID and resending token so we can use them later
+                storedVerificationId = verificationId
+                resendToken = token
 
 
 
-                    }
+
+
+            }
 
 //                    val credential = PhoneAuthProvider.getCredential(storedVerificationId!!, et_otp.text.toString())
 
-            private fun signInWithPhoneAuthCredential(credential: PhoneAuthCredential) {
-                auth.signInWithCredential(credential)
-                    .addOnCompleteListener(this@Signup) { task ->
-                        if (task.isSuccessful) {
-                            // Sign in success, update UI with the signed-in user's information
-                            Log.d(TAG, "signInWithCredential:success")
-
-                            val user = task.result?.user
-                        } else {
-                            // Sign in failed, display a message and update the UI
-                            Log.w(TAG, "signInWithCredential:failure", task.exception)
-                            if (task.exception is FirebaseAuthInvalidCredentialsException) {
-                                // The verification code entered was invalid
-                            }
-                            // Update UI
-                        }
-                    }
-            }
 
 
-        })          // OnVerificationStateChangedCallbacks
+
+
+
+        }
+
+        btn_smsSent.setOnClickListener(){
+            val options = PhoneAuthOptions.newBuilder(auth)
+                .setPhoneNumber("+82"+et_phone.text.toString()) //국가 번호 +82 를 해줬음
+                .setTimeout(60L, TimeUnit.SECONDS)
+                .setActivity(this)
+                .setCallbacks(callbacks)
+
+                            // OnVerificationStateChangedCallbacks
                 .build()
             PhoneAuthProvider.verifyPhoneNumber(options)
 
         }
 
+        fun verifyPhoneNumberWithCode(){
+            var credential = PhoneAuthProvider.getCredential(storedVerificationId, et_otp.text.toString())
+            signInWithPhoneAuthCredential(credential)
+        }
 
-
+        btn_verify.setOnClickListener(){
+            verifyPhoneNumberWithCode()
+        }
 
 
 
