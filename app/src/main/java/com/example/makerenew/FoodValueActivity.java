50b50889837193bf450cbd7a687368954114b478
AngelPlayer <ssh5212@naver.com>
2022-01-19 13:42:57 +0900

    음식 수치 화면 수정

---
+++ app/src/main/java/com/example/makerenew/FoodValueActivity.java
@@ -0,0 +1,14 @@
+package com.example.makerenew;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FoodValueActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_food_value);
+    }
+}
