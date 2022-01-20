a18155e67fc30f14a97e14217b40b50e09cc2237
AngelPlayer <ssh5212@naver.com>
2022-01-20 11:21:06 +0900

    Food Class 구현

---
+++ app/src/main/java/com/example/makerenew/Food.java
@@ -0,0 +1,155 @@
+package com.example.makerenew;
+
+public class Food {
+    String classification;
+
+    int meal_tan = 0;
+    int meal_dan = 0;
+    int meal_ge = 0;
+    int meal_cal = 0;
+
+    String meal_food1 = "-";
+    String meal_food2 = "-";
+    String meal_food3 = "-";
+    String meal_food4 = "-";
+    String meal_food5 = "-";
+
+    String district1 = "-";
+    String district2 = "-";
+    String district3 = "-";
+    String district4 = "-";
+    String district5 = "-";
+    String district6 = "-";
+
+    public Food(String classification) {
+        this.classification = classification;
+    }
+
+    public String getClassification() {
+        return classification;
+    }
+
+    public void setClassification(String classification) {
+        this.classification = classification;
+    }
+
+    public int getMeal_tan() {
+        return meal_tan;
+    }
+
+    public void setMeal_tan(int meal_tan) {
+        this.meal_tan = meal_tan;
+    }
+
+    public int getMeal_dan() {
+        return meal_dan;
+    }
+
+    public void setMeal_dan(int meal_dan) {
+        this.meal_dan = meal_dan;
+    }
+
+    public int getMeal_ge() {
+        return meal_ge;
+    }
+
+    public void setMeal_ge(int meal_ge) {
+        this.meal_ge = meal_ge;
+    }
+
+    public int getMeal_cal() {
+        return meal_cal;
+    }
+
+    public void setMeal_cal(int meal_cal) {
+        this.meal_cal = meal_cal;
+    }
+
+    public String getMeal_food1() {
+        return meal_food1;
+    }
+
+    public void setMeal_food1(String meal_food1) {
+        this.meal_food1 = meal_food1;
+    }
+
+    public String getMeal_food2() {
+        return meal_food2;
+    }
+
+    public void setMeal_food2(String meal_food2) {
+        this.meal_food2 = meal_food2;
+    }
+
+    public String getMeal_food3() {
+        return meal_food3;
+    }
+
+    public void setMeal_food3(String meal_food3) {
+        this.meal_food3 = meal_food3;
+    }
+
+    public String getMeal_food4() {
+        return meal_food4;
+    }
+
+    public void setMeal_food4(String meal_food4) {
+        this.meal_food4 = meal_food4;
+    }
+
+    public String getMeal_food5() {
+        return meal_food5;
+    }
+
+    public void setMeal_food5(String meal_food5) {
+        this.meal_food5 = meal_food5;
+    }
+
+    public String getDistrict1() {
+        return district1;
+    }
+
+    public void setDistrict1(String district1) {
+        this.district1 = district1;
+    }
+
+    public String getDistrict2() {
+        return district2;
+    }
+
+    public void setDistrict2(String district2) {
+        this.district2 = district2;
+    }
+
+    public String getDistrict3() {
+        return district3;
+    }
+
+    public void setDistrict3(String district3) {
+        this.district3 = district3;
+    }
+
+    public String getDistrict4() {
+        return district4;
+    }
+
+    public void setDistrict4(String district4) {
+        this.district4 = district4;
+    }
+
+    public String getDistrict5() {
+        return district5;
+    }
+
+    public void setDistrict5(String district5) {
+        this.district5 = district5;
+    }
+
+    public String getDistrict6() {
+        return district6;
+    }
+
+    public void setDistrict6(String district6) {
+        this.district6 = district6;
+    }
+}
