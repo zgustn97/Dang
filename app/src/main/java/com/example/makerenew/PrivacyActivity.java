9d0a6ba3b1272c6b8b65a8ea782118d9ea332b51
AngelPlayer <ssh5212@naver.com>
2022-01-20 19:48:26 +0900

    1주차 코드 추가

---
+++ app/src/main/java/com/example/makerenew/PrivacyActivity.java
@@ -0,0 +1,44 @@
+package com.example.makerenew;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class PrivacyActivity extends AppCompatActivity {
+
+    String N = null; //이름
+    String NM = null; //닉네임
+    String B = null;  //생년월일
+    String P = null; //전화번호
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_privacy);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
+
+        Button next = (Button) findViewById(R.id.next);
+
+        EditText nameText = (EditText)findViewById(R.id.nameText);
+        EditText nicknameText = (EditText)findViewById(R.id.nicknameText);
+        EditText birthText = (EditText)findViewById(R.id.birthText);
+        EditText phoneText = (EditText)findViewById(R.id.phoneText);
+        View.OnClickListener listener = new View.OnClickListener() //입력 버튼 누르면 입력된 데이터 각 변수에 저장
+        {
+            @Override
+            public void onClick(View v) {
+                N = nameText.getText().toString();
+                NM = nicknameText.getText().toString();
+                B = birthText.getText().toString();
+                P = phoneText.getText().toString();
+            }
+        };
+        next.setOnClickListener(listener);
+    }
+}
---
+++ app/src/main/java/com/example/makerenew/PrivacyHealthActivity.java
@@ -0,0 +1,113 @@
+package com.example.makerenew;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.MultiAutoCompleteTextView;
+import android.widget.MultiAutoCompleteTextView.CommaTokenizer;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+public class PrivacyHealthActivity extends AppCompatActivity {
+    String W = null; //몸무게
+    String H = null; //키
+    String D = null;  //당뇨유형
+    String HD = null; //혈당강화제
+    String I = null;  //안슐린
+    String M = null;  //처방약
+    String GL = null;  //GLP 주사
+    String G1 = null;  //목표혈당 1
+    String G2 = null;   //목표혈당 2
+    int F=0; //식사량 숫자 1,2,3 으로 저장 1이 적음 2가 보통 3이 많음
+    int E=0; //운동량
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_privacy_health);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
+
+        Button enter = (Button) findViewById(R.id.enter);
+
+        EditText weight = (EditText)findViewById(R.id.weight);
+        EditText height = (EditText)findViewById(R.id.height);
+        Spinner 당뇨유형 = (Spinner)findViewById(R.id.당뇨유형);
+        Spinner 혈당강화제 = (Spinner)findViewById(R.id.혈당강화제);
+        Spinner 인슐린 = (Spinner)findViewById(R.id.인슐린);
+        EditText 처방약 = (EditText)findViewById(R.id.처방약);
+        Spinner GLP = (Spinner)findViewById(R.id.GLP);
+        EditText 목표혈당 = (EditText)findViewById(R.id.목표혈당);
+        EditText 목표혈당2 = (EditText)findViewById(R.id.목표혈당2);
+
+        RadioGroup radioGroup = findViewById(R.id.radioGroup);
+        RadioGroup radioGroup2 = findViewById(R.id.radioGroup2);
+
+        View.OnClickListener listener = new View.OnClickListener() //입력 버튼 누르면 입력된 데이터 각 변수에 저장
+        {
+            @Override
+            public void onClick(View v) {
+                W = weight.getText().toString();
+                H = height.getText().toString();
+                D = 당뇨유형.getSelectedItem().toString();
+                HD = 혈당강화제.getSelectedItem().toString();
+                I = 인슐린.getSelectedItem().toString();
+                M = 처방약.getText().toString();
+                GL = GLP.getSelectedItem().toString();
+                G1= 목표혈당.getText().toString();
+                G2 = 목표혈당2.getText().toString();
+
+            }
+        };
+        enter.setOnClickListener(listener);
+
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() { // 식사량
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                switch (checkedId){
+                    case R.id.rgBtn1:
+                        F=1;
+                        break;
+                    case R.id.rgBtn2:
+                        F=2;
+                        break;
+                    case R.id.rgBtn3:
+                        F=3;
+                        break;
+                }
+            }
+        });
+        radioGroup2.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() { // 운동량
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                switch (checkedId){
+                    case R.id.rgBtn2_1:
+                        F=1;
+                        break;
+                    case R.id.rgBtn2_2:
+                        F=2;
+                        break;
+                    case R.id.rgBtn2_3:
+                        F=3;
+                        break;
+                }
+            }
+        });
+
+    }
+}
